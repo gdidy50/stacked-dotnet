4ac7219b1de385d1f0f3c53618d24572de5049f9
Grant Burke <gdidy50@gmail.com>
2020-12-31 14:16:50 -0600

    Added update/delete methods to ArticleController

--- Stacked.API/Controllers/ArticleController.cs
+++ Stacked.API/Controllers/ArticleController.cs
@@ -100,5 +100,57 @@ namespace Stacked.API.Controllers
                     new { id = newArticle.Data }
                 );
         }
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdateArticle(string id, [FromBody] ArticleDto article)
+        {
+            try
+            {
+                var guid = Guid.Parse(id);
+                var updatedArticle = await _articleService.Update(guid, article);
+                if (updatedArticle.Error != null)
+                {
+                    _logger.LogError($"Error updating Article {id}: {updatedArticle.Error.Message}");
+                    return StatusCode(
+                        StatusCodes.Status500InternalServerError,
+                        "There was an error updating the Article"
+                    );
+                }
+
+                _logger.LogDebug($"Updated Article: {id}");
+                return Ok(updatedArticle.Data.Id);
+            }
+            catch (FormatException ex)
+            {
+                _logger.LogWarning($"There was a GUID format error for article {id}\n{ex.Message}\n{ex.StackTrace}");
+                return BadRequest(id);
+            }
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<ActionResult> DeleteArticle(string id)
+        {
+            try
+            {
+                var guid = Guid.Parse(id);
+                var deleted = await _articleService.Delete(guid);
+                if (deleted.Error != null)
+                {
+                    _logger.LogError($"Error deleted Article {id}: {deleted.Error.Message}");
+                    return StatusCode(
+                        StatusCodes.Status500InternalServerError,
+                        "There was an error deleted the Article"
+                    );
+                }
+
+                _logger.LogDebug($"Updated Article: {id}");
+                return Ok(deleted.Data);
+            }
+            catch (FormatException ex)
+            {
+                _logger.LogWarning($"There was a GUID format error for article {id}\n{ex.Message}\n{ex.StackTrace}");
+                return BadRequest(id);
+            }
+        }
     }
 }
